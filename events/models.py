3ef32b1eec7f5f3f25b4b0bc1033be86c7fbeb68
Mostafa <mostafa.saneisn@gmail.com>
2024-06-26 20:35:14 +0330

    Feat: add Event model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    date = models.DateTimeField()
+    location = models.CharField(max_length=300)
+    description = models.TextField(default=None)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
