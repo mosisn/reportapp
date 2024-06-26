486bc4690df357483263bf9114e4be67f4a32443
Mostafa <mostafa.saneisn@gmail.com>
2024-06-26 20:39:57 +0330

    Feat: add upcoming events view

--- events/views.py
+++ events/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Event
+from django.utils import timezone
+from datetime import timedelta
 
-# Create your views here.
+
+def upcoming_events(request):
+    now = timezone.now() 
+    date = now - timedelta(days=7)
+    data = Event.objects.filter(date__gt=date).order_by('-date')
+    
+    context = {
+        'data' : data,
+        'now' : now
+    }
+    return render(request,'upcoming_events.html', context)
